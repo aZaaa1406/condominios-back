0548fc0d52bc2baaf95d2445c4be917fc3801352
aZaaa1406 <rvaldeza2200@alumno.ipn.mx>
2025-07-16 16:01:17 -0600

    ayuyax3

--- app/utils/googleCloud.js
+++ app/utils/googleCloud.js
@@ -4,8 +4,10 @@ import { GCS } from '../config/config.js';
 // Ruta absoluta al archivo JSON
 const serviceAccount = JSON.parse(GCS || '{}');
 
+
+console.log(serviceAccount);
 const storage = new Storage({
-  credentials: GCS,
+  credentials: serviceAccount,
   projectId: 'eventspace-442007' // lo encuentras en el archivo JSON
 });
 
